1b510da06f5a4d278ce95e851486ccd9b7fce320
Sneha Srivastava <srivastavasneha895@gmail.com>
2025-10-15 12:27:54 +0000

    Day01 : Hello World and Variables

---
+++ day01_basics/helloworld.py
@@ -0,0 +1,8 @@
+#Topic : Python Basics
+#basic code to print hello
+print("Hello, Python-a2z!")
+
+#variables
+name = "Sneha"
+area = "Data Science"
+print(f"My name is {name} and I'm passionate about {area} ")
